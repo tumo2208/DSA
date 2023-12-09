e08df0d034e438f45062b0acddd186416225ed69
tumo2208 <tuadc1234@gmail.com>
2023-12-09 20:27:50 +0700

    fix dfs and comment dijkstra

--- src/Graphs/DFSAlgorithm.java
+++ src/Graphs/DFSAlgorithm.java
@@ -1,18 +1,14 @@
 package Graphs;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class DFSAlgorithm {
     private List<Integer> result;           //List các đỉnh được sắp xếp theo DFS
     private boolean[] marked;               //List bool xác định xem 1 đỉnh đã được thăm hay chưa
-    private Stack<Integer> st;              //Stack lưu các đỉnh liền kề với các đỉnh đã được thăm
 
     public DFSAlgorithm(Graph graph, int v) {
         result = new ArrayList<>();
         marked = new boolean[graph.V()];
-        st = new Stack<>();
         DFS(graph, v);
     }
 
@@ -22,17 +18,13 @@ public class DFSAlgorithm {
         result.add(v);
 
         //Thêm các đỉnh liền kề với đỉnh vừa được thăm vào stack
-        for (Edge edge : graph.adj(v)) {
-            if (!marked[edge.other(v)] && !st.contains(edge.other(v)))
-                st.push(edge.other(v));
-        }
+        Iterator<Edge> ite = graph.adj(v).listIterator();
 
         //Nếu stack vẫn chưa rỗng (dfs chưa duyệt qua tất cả các đỉnh)
         //Duyệt đỉnh được thêm vào stack gần nhất
-        while (!st.isEmpty()) {
-            int index = st.pop();
+        while (ite.hasNext()) {
+            int index = ite.next().other(v);
             if (!marked[index]) {
-                marked[index] = true;
                 DFS(graph, index);
             }
         }
--- src/Graphs/Dijkstra.java
+++ src/Graphs/Dijkstra.java
@@ -2,9 +2,10 @@ package Graphs;
 
 import java.util.PriorityQueue;
 
+// PriorityQueue có index
 class Pair implements Comparable<Pair> {
-    private int index;
-    private double weight;
+    private int index;                      // index
+    private double weight;                  // value
 
     public Pair(int index, double weight) {
         this.index = index;
@@ -27,8 +28,8 @@ class Pair implements Comparable<Pair> {
 
 
 public class Dijkstra {
-    private double[] shortestPath;
-    private PriorityQueue<Pair> pq;
+    private double[] shortestPath;              // Mảng lưu đường đi ngắn nhất từ 1 đỉnh đến các đỉnh còn lại
+    private PriorityQueue<Pair> pq;             // PriorityQueue lưu các giá trị khi xét qua mỗi đỉnh
 
     public Dijkstra(Graph graph, int s) {
         shortestPath = new double[graph.V()];
@@ -38,9 +39,11 @@ public class Dijkstra {
             shortestPath[i] = Double.POSITIVE_INFINITY;
         }
 
+        // Bắt đầu đi từ đỉnh s
         shortestPath[s] = 0.0;
         pq.add(new Pair(s, shortestPath[s]));
 
+        // Khi pq chx rỗng, lấy ra giá trị nhỏ nhất của pq và duyệt các đỉnh liền kề với nó
         while (!pq.isEmpty()) {
             Pair p = pq.remove();
             int v = p.getIndex();
@@ -50,20 +53,24 @@ public class Dijkstra {
         }
     }
 
+    // Duyệt các đỉnh liền kề
     private void relax(Edge e, int v) {
-        int  w = e.other(v);
+        int  w = e.other(v);        // w là đỉnh liền kề với v
+
+        // Nếu đường đi từ s - w hiện tại có giá trị lớn hơn đường đi từ s - v công với cạnh vw
         if (shortestPath[w] > shortestPath[v] + e.getWeight()) {
-            shortestPath[w] = shortestPath[v] + e.getWeight();
+            shortestPath[w] = shortestPath[v] + e.getWeight();          // Thay đổi giá trị đường đi từ s - w
             for (Pair pair : pq) {
                 if (pair.getIndex() == w) {
                     pq.remove(pair);
                     break;
                 }
             }
-            pq.add(new Pair(w, shortestPath[w]));
+            pq.add(new Pair(w, shortestPath[w]));                       // Thêm giá trị đó vào pq và xóa giá trị trc đó của đỉnh w
         }
     }
 
+    // Lấy ra trọng số của đường đi ngắn nhất từ đỉnh s - w
     public double shortestPath(int w) {
         return shortestPath[w];
     }
